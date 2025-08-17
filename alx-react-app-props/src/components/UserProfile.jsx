6d0c8096543d6042b4911b250e0daec8c7bb0863
Phethile Nkosi <phethileayanda58@gmail.com>
2025-08-17 18:27:40 +0200

    Added  UserProfile.jsx to directory

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,16 +1,12 @@
-import { useContext } from "react";
-import UserContext from "./UserContext";
-
-function UserProfile() {
-  const userData = useContext(UserContext);
-
-  return (
-    <div>
-      <h2>User Profile</h2>
-      <p>Name: {userData.name}</p>
-      <p>Email:{userData.email}</p>
-    </div>
-  );
-}
+import React from 'react';
+const UserProfile = (props) => {
+   return (
+     <div>
+       <h2>{props.name}</h2>
+       <p>Age: {props.age}</p>
+       <p>Bio: {props.bio}</p>
+     </div>
+   );
+ };
 
  export default UserProfile;

0d9046a8e4dabbea6bfc4d14ca080c46d3fb301f
Phethile Nkosi <phethileayanda58@gmail.com>
2025-08-21 18:25:49 +0200

    Added Search.jsx and app.jsx to directory

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -3,20 +3,20 @@ import { fetchUsersData } from "../services/githubService";
 
 const  Search = () => {
   const [username, setUsername] = useState("");
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-     if (!username)
+     if (!username) return;
 
     setLoading(true);
     setError("");
     setUser(null);
 
     try {
-      const data = await fetchU(username);
+      const data = await fetchUserData(username);
       setUser(data);
     } catch (err) {
      setError("Looks like we cant find the user");
@@ -27,7 +27,6 @@ const  Search = () => {
 
   return (
    <div className="max-w-xl mx-auto p-4">
-
     <form onSubmit={handleSearch} className="flex gap-2 mb-4">
       <input
         type="text"
@@ -43,7 +42,7 @@ const  Search = () => {
           Search
         </button>
       </form>
-{/* Conditional Rendering */}
+      
       {loading && <p className="text-gray-500">Loading...</p>}
       {error && <p className="text-red-500">{error}</p>}
       {user && (

3b406dc6de853ddc4f2e87acc5a4e3025e82e718
Phethile Nkosi <phethileayanda58@gmail.com>
2025-08-22 14:19:02 +0200

    Added Search.jsx to directory

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import { fetchUsersData, fetchAdvanceUsers} from "../services/githubService";
+import { fetchAdvanceUsers,  fetchUserData} from "../services/githubService";
 
 const  Search = () => {
   const [username, setUsername] = useState("");

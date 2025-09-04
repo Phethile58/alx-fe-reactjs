d3d9bda81e2b6e175feab466721684bac6e939ea
Phethile Nkosi <phethileayanda58@gmail.com>
2025-09-04 20:21:52 +0200

    Enhance UserProfile component with hover effects, transitions, and animations using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,20 +1,15 @@
 export default function UserProfile() {
   return (
     <div className="mx-auto my-10 max-w-xs md:max-w-sm bg-gray-100 rounded-lg shadow-lg p-4 sm:p-4 md:p-8 text-center">
-      {/* Responsive Profile Image */}
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="mx-auto rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36"
       />
-
-      {/* Responsive Heading */}
-      <h1 className="my-4 text-lg sm:text-lg md:text-xl text-blue-800 font-semibold">
+      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800  my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">
         John Doe
       </h1>
-
-      {/* Responsive Paragraph */}
-      <p className="text-sm md:text-base text-gray-600">
+      <p className="text-sm sm:text-sm md:text-base text-gray-600 text-center">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>

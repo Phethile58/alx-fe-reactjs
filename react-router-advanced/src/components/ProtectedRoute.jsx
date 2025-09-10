b6149b4a13764cca2b43abf31dc8ac35b55aa97e
Phethile Nkosi <phethileayanda58@gmail.com>
2025-09-10 20:22:13 +0200

    Add React Router setup with dynamic and protected routes

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
-export default function ProtectedRoute({ isAuth, children }) {
-  if (!isAuth) {
-    return <Navigate to="/login" />;
+export default function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    // Redirect unauthenticated users to home page
+    return <Navigate to="/" replace />;
   }
+
   return children;
 };
 
